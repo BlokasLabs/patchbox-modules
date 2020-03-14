9cb8df1d9d0d429af0bda76b6ffcc3f9b028fb4d
Giedrius <giedrius@blokas.io>
2020-03-14 10:11:47 +0000

    Install pisound-ctl-scripts-puredata with puredata

--- puredata/install.sh
+++ puredata/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SOFTWARE_TO_INSTALL="puredata pisound-ctl amidiauto"
+SOFTWARE_TO_INSTALL="puredata pisound-ctl amidiauto pisound-ctl-scripts-puredata"
 sudo apt-get update
 sudo apt-get install $SOFTWARE_TO_INSTALL -y
 echo "Done! Thank you!"
