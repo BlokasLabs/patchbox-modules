7609d70b22a1e3860e61263145a5589ae9f72ed1
Giedrius <giedrius@blokas.io>
2024-04-02 11:43:43 +0100

    Updated for bookworm

--- puredata/install.sh
+++ puredata/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SOFTWARE_TO_INSTALL="puredata pisound-ctl amidiauto pisound-ctl-scripts-puredata"
+SOFTWARE_TO_INSTALL="puredata pisound-ctl amidiminder pisound-ctl-scripts-puredata"
 sudo apt-get update
 sudo apt-get install $SOFTWARE_TO_INSTALL -y
 echo "Done! Thank you!"
